39eb320b190e095a17910d2768e6e8c8b9e8cf9d
Jordan Adams <jcadamsboomer@gmail.com>
2023-08-10 08:13:47 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const writeCards = (arr,event) => {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+        }
+    return messages
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+function countDown() {
+let countDown = 10;
+while (countDown >= 0) {
+    console.log(countDown--);
+}
+}
